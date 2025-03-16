a5981bf4aa1de5ce765be59af850f7cc9b15844d
vladislav_garaev <vladislav_garaev@epam.com>
2025-03-16 11:21:35 +0100

    feat: add search-form.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import {Counter} from "./components/counter.tsx";
+import {SearchForm} from "./components/search-form.tsx";
 
 function App() {
     return (
-        <>
+        <div className="flex flex-col gap-4">
+            <SearchForm onSearch={console.log} initialQuery="Star Wars"/>
             <div className="card">
                 <Counter initialValue={42}/>
             </div>
-        </>
+        </div>
     )
 }
 
